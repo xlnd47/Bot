ee6a063521157568d378954bb852ebb57abf6b7c
xlnd47 <axel.deschuijmer@gmail.com>
2018-12-14 15:38:44 +0100

    Update mycog.py

--- cogs/mycog.py
+++ cogs/mycog.py
@@ -1,4 +1,5 @@
 import discord
+import requests
 from discord.ext import commands
 
 class Mycog:
@@ -20,6 +21,14 @@ class Mycog:
 
         #Your code will go here
         await self.bot.say("ONE PUNCH! And " + user.mention + " is out! ლ(ಠ益ಠლ)")
+
+    @commands.command()
+    async def champrot(self, user : discord.Member):
+        """Champion rotation LoL"""
+
+        #Your code will go here
+        response = requests.get("https://euw1.api.riotgames.com/lol/platform/v3/champion-rotations")
+        await self.bot.say(response.status_code)
     
 
 def setup(bot):
