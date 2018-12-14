20305ef6763feace4bccf7c244f52fe1208d31ca
xlnd47 <axel.deschuijmer@gmail.com>
2018-12-14 15:20:42 +0100

    Create mycog.py

---
+++ cogs/mycog.py
@@ -0,0 +1,26 @@
+import discord
+from discord.ext import commands
+
+class Mycog:
+    """My custom cog that does stuff!"""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def mycom(self):
+        """This does stuff!"""
+
+        #Your code will go here
+        await self.bot.say("I can do stuff!")
+
+    @commands.command()
+    async def punch(self, user : discord.Member):
+        """I will punch anyone! >.<"""
+
+        #Your code will go here
+        await self.bot.say("ONE PUNCH! And " + user.mention + " is out! ლ(ಠ益ಠლ)")
+    
+
+def setup(bot):
+    bot.add_cog(Mycog(bot))
